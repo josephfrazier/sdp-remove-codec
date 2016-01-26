05e77be2a3a6afda927574a6c599c9a2a67adc59
Joseph Frazier <1212jtraceur@gmail.com>
2016-01-26 08:30:04 -0500

    tests: Improve helper function argument order

--- test.js
+++ test.js
@@ -5,37 +5,37 @@ var fs = require('fs')
 var input = fs.readFileSync('./sdp.txt', 'utf8')
 
 test('removes a=rtpmap line', function (t) {
-  removesLine(input, 111, 'a=rtpmap:111 opus/48000/2', t)
+  removesLine(t, input, 111, 'a=rtpmap:111 opus/48000/2')
   t.end()
 })
 
 test('removes a=fmtp line', function (t) {
-  removesLine(input, 111, 'a=fmtp:111 minptime=10; useinbandfec=1', t)
+  removesLine(t, input, 111, 'a=fmtp:111 minptime=10; useinbandfec=1')
   t.end()
 })
 
 test('removes payload type from m=audio line', function (t) {
   var badLine = 'm=audio 56333 UDP/TLS/RTP/SAVPF 111 103 104 9 0 8 106 105 13 126'
   var goodLine = 'm=audio 56333 UDP/TLS/RTP/SAVPF 103 104 9 0 8 106 105 13 126'
-  replacesLine(input, 111, badLine, goodLine, t)
+  replacesLine(t, input, 111, badLine, goodLine)
   t.end()
 })
 
 test('removes a=rtcp-fb lines', function (t) {
-  removesLine(input, 100, 'a=rtcp-fb:100 ccm fir', t)
-  removesLine(input, 100, 'a=rtcp-fb:100 nack', t)
-  removesLine(input, 100, 'a=rtcp-fb:100 nack pli', t)
-  removesLine(input, 100, 'a=rtcp-fb:100 goog-remb', t)
+  removesLine(t, input, 100, 'a=rtcp-fb:100 ccm fir')
+  removesLine(t, input, 100, 'a=rtcp-fb:100 nack')
+  removesLine(t, input, 100, 'a=rtcp-fb:100 nack pli')
+  removesLine(t, input, 100, 'a=rtcp-fb:100 goog-remb')
   t.end()
 })
 
-function removesLine (input, payloadType, badLine, t) {
+function removesLine (t, input, payloadType, badLine) {
   var output = sdpRemoveCodec(payloadType, input)
   t.ok(input.includes(badLine))
   t.notOk(output.includes(badLine))
 }
 
-function replacesLine (input, payloadType, badLine, goodLine, t) {
+function replacesLine (t, input, payloadType, badLine, goodLine) {
   var output = sdpRemoveCodec(payloadType, input)
 
   t.ok(input.includes(badLine))
