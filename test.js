91e82cb30dae892297f2ec9edc0d9ec4f5933f05
Joseph Frazier <1212jtraceur@gmail.com>
2016-01-26 08:16:22 -0500

    Dedupe tests into helper functions

--- test.js
+++ test.js
@@ -4,35 +4,36 @@ var fs = require('fs')
 
 test('removes a=rtpmap line', function (t) {
   var input = fs.readFileSync('./sdp.txt', 'utf8')
-  var output = sdpRemoveCodec(111, input)
-  var badLine = 'a=rtpmap:111 opus/48000/2'
-
-  t.plan(2)
-  t.ok(input.includes(badLine))
-  t.notOk(output.includes(badLine))
+  removesLine(input, 111, 'a=rtpmap:111 opus/48000/2', t)
+  t.end()
 })
 
 test('removes a=fmtp line', function (t) {
   var input = fs.readFileSync('./sdp.txt', 'utf8')
-  var output = sdpRemoveCodec(111, input)
-  var badLine = 'a=fmtp:111 minptime=10; useinbandfec=1'
-
-  t.plan(2)
-  t.ok(input.includes(badLine))
-  t.notOk(output.includes(badLine))
+  removesLine(input, 111, 'a=fmtp:111 minptime=10; useinbandfec=1', t)
+  t.end()
 })
 
 test('removes payload type from m=audio line', function (t) {
   var input = fs.readFileSync('./sdp.txt', 'utf8')
-  var output = sdpRemoveCodec(111, input)
   var badLine = 'm=audio 56333 UDP/TLS/RTP/SAVPF 111 103 104 9 0 8 106 105 13 126'
   var goodLine = 'm=audio 56333 UDP/TLS/RTP/SAVPF 103 104 9 0 8 106 105 13 126'
+  replacesLine(input, 111, badLine, goodLine, t)
+  t.end()
+})
 
-  t.plan(4)
+function removesLine (input, payloadType, badLine, t) {
+  var output = sdpRemoveCodec(payloadType, input)
+  t.ok(input.includes(badLine))
+  t.notOk(output.includes(badLine))
+}
+
+function replacesLine (input, payloadType, badLine, goodLine, t) {
+  var output = sdpRemoveCodec(payloadType, input)
 
   t.ok(input.includes(badLine))
   t.notOk(input.includes(goodLine))
 
   t.ok(output.includes(goodLine))
   t.notOk(output.includes(badLine))
-})
+}
